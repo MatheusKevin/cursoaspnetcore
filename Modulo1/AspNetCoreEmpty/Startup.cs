7812b4b6703b5b44f3005ae14531039edfc76c6f
Matheus Pelegrini <kevin.pelegrini@outlook.com>
2020-03-16 11:51:06 -0300

    leitura de arquivo de configuração

--- Modulo1/AspNetCoreEmpty/Startup.cs
+++ Modulo1/AspNetCoreEmpty/Startup.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreEmpty
 {
     public class Startup
     {
+        public IConfiguration _config { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +40,12 @@ namespace AspNetCoreEmpty
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var mensagem = _config["Mensagem"];
+                var conexao = _config["ConnectionStrings:DefaultConnection"];
+
+                // await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(mensagem);
+                await context.Response.WriteAsync(conexao);
             });
         }
     }
